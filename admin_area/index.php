aa9a43342f995a74494de88da7ba0b777bcfc4a1
abdullah <abdullah.work06@gmail.com>
2022-09-28 05:43:43 +0200

    adding navbar to admin

--- admin_area/index.php
+++ admin_area/index.php
@@ -8,13 +8,99 @@
     <!-- Bootstrap link -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <!-- Style link -->
-    <link rel="stylesheet" href="./styles/style.css">
+    <link rel="stylesheet" href="../styles/style.css">
 </head>
 <body>
     
+    <div class="container-fluid p-0">
+        <!-- first child -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-info bg-gradient">
+            <div class="container-fluid">
+                <img src="../images/logo/logo-2.png" alt="void-logo" class="logo">
+                <nav class="navbar navbar-expand-lg">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">xW3lpy-1 come guest</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </nav>
+        <!-- second child -->
+        <div class="bg-light bg-gradient">
+            <h3 class="text-center p-2">
+                Manage Details
+            </h3>
+        </div>
+        <!-- Third child -->
+        <div class="row m-0">
+            <div class="col-md-12 bg-secondary bg-gradient d-flex p-1 align-items-center">
+                <div class="p-3">
+                    <a href="#" >
+                        <img src="../images/products/pinapple.jpg" class="admin_image" alt="Pineapple Juice">
+                    </a>
+                    <p class="text-light text-center">
+                        Admin Name
+                    </p>
+                </div>
+                <div class="button d-flex flex-wrap gap-2">
+                    <button>
+                            <a href="#" class="nav-link px-3 py-1  py-1 text-light bg-info bg-gradient my-1 ">
+                                Insert Products
+                            </a>
+                    </button>
+                    <button>
+                            <a href="#" class="nav-link px-3 py-1 text-light bg-info bg-gradient my-1">
+                                View Products
+                            </a>
+                    </button>
+                    <button>
+                            <a href="#" class="nav-link px-3 py-1 text-light bg-info bg-gradient my-1">
+                                Insert Categories
+                            </a>
+                    </button>
+                    <button>
+                            <a href="#" class="nav-link px-3 py-1 text-light bg-info bg-gradient my-1">
+                                View Categories
+                            </a>
+                    </button>
+                    <button>
+                            <a href="#" class="nav-link px-3 py-1 text-light bg-info bg-gradient my-1">
+                                Insert Brands
+                            </a>
+                    </button>
+                    <button>
+                            <a href="#" class="nav-link px-3 py-1 text-light bg-info bg-gradient my-1">
+                                View Brands
+                            </a>
+                    </button>
+                    <button>
+                            <a href="#" class="nav-link px-3 py-1 text-light bg-info bg-gradient my-1">
+                                All Orders        
+                            </a>
+                    </button>
+                    <button>
+                            <a href="#" class="nav-link px-3 py-1 text-light bg-info bg-gradient my-1">
+                                All Payments
+                            </a>
+                    </button>
+                    <button>
+                            <a href="#" class="nav-link px-3 py-1 text-light bg-info bg-gradient my-1">
+                                List Users
+                            </a>
+                    </button>
+                    <button>
+                            <a href="#" class="nav-link px-3 py-1 text-light bg-info bg-gradient my-1">
+                                Logout
+                            </a>
+                    </button>
 
+                </div>
+            </div>
+        </div>
+    </div>
     <!-- Last child -->
-    <footer class="bg-info p-4 text-center">
+    <footer class="bg-info bg-gradient p-4 text-center footer">
         <p class="m-0">All rights reserved &copy; Designed by Abdullah Ahmed-2022</p>
     </footer>
     <!-- Bootstrap js link -->
--- index.php
+++ index.php
@@ -12,10 +12,10 @@
     <!-- Style link -->
     <link rel="stylesheet" href="./styles/style.css">
 </head>
-<body>
+<>
     <header class="container-fluid p-0">
         <!-- first child  Navigation-->
-       <nav class="navbar navbar-expand-lg bg-info">
+       <nav class="navbar navbar-expand-lg bg-info bg-gradient">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">
                     <img src="./images/logo/logo-2.png" class="logo" alt="Ecommerce logo">
@@ -57,7 +57,7 @@
     </header>
 
     <!-- Second Child  Greeting|login-->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary p-2">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary bg-gradient p-2">
         <ul class="navbar-nav me-auto">
             <li class="nav-item">
                 <a href="#" class="nav-link">Welcome guest</a>
@@ -70,7 +70,7 @@
     </nav>
 
     <!-- Third Child  Store name-->
-    <div class="bg-light p-2">
+    <div class="bg-light bg-gradient p-2">
         <h3 class="text-center">
             Void Store
         </h3>
@@ -81,10 +81,10 @@
 
     <!-- Fourth Child Products and Sidenav -->
     <div class="row m-0">
-        <aside class="col-md-2 bg-secondary p-0">
+        <aside class="col-md-2 bg-secondary bg-gradient p-0">
             <!-- Brands to be displayed -->
             <ul class="navbar-nav me-auto text-center">
-                <li class="nav-item bg-info mb-1">
+                <li class="nav-item bg-info bg-gradient mb-1">
                     <a href="#" class="nav-link text-light">
                         <h4>Delivery Brand</h4>
                     </a>
@@ -118,7 +118,7 @@
 
             <!-- Categories to be displayed -->
             <ul class="navbar-nav me-auto text-center">
-                <li class="nav-item bg-info mb-1">
+                <li class="nav-item bg-info bg-gradient mb-1">
                     <a href="#" class="nav-link text-light">
                         <h4>Categories</h4>
                     </a>
@@ -264,5 +264,5 @@
     </footer>
     <!-- Bootstrap js link -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-</body>
+</ bg-gradientbody>
 </html>
