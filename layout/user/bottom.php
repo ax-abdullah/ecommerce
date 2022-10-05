d728d4fadca9385c297c4a8783a02ac5fdd02fbc
abdullah <abdullah.work06@gmail.com>
2022-10-05 04:14:43 +0200

    adding reusable footer and html end tags

---
+++ layout/user/bottom.php
@@ -0,0 +1,9 @@
+    <!-- Last child -->
+    <footer class="bg-info p-4 text-center">
+        <p class="m-0">All rights reserved &copy; Designed by Abdullah Ahmed-2022</p>
+    </footer>
+    <!-- Bootstrap js link -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</body>
+
+</html>
